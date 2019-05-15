d59b162bcb00f9ad8deab4523c3ad0f6162c812c
Tuan Duong <tuan@eatsyapp.co>
2019-05-16 01:02:24 +0700

    RT: Create project structure RT: Create Entityframework Structure

---
+++ Web/ResellTicketSolution/Core/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,54 @@
+﻿using Core.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Core.Infrastructure
+{
+    public interface IDatabaseFactory : IDisposable
+    {
+        ResellTicketDbContext Get();
+        ResellTicketDbContext GetNew();
+    }
+
+    public class DatabaseFactory : Disposable, IDatabaseFactory
+    {
+        private ResellTicketDbContext _dataContext;
+        private IConfiguration _configuration;
+
+        public DatabaseFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ResellTicketDbContext Get()
+        {
+            if(_dataContext == null)
+            {
+                var dbOptionsBuilder = new DbContextOptionsBuilder<ResellTicketDbContext>();
+                dbOptionsBuilder.UseSqlServer(_configuration.GetConnectionString("ResellTicketDB"));
+
+                _dataContext = new ResellTicketDbContext(dbOptionsBuilder.Options);
+            }
+
+            return _dataContext;
+        }
+
+        public ResellTicketDbContext GetNew()
+        {
+            _dataContext?.Dispose();
+
+            var dbOptionsBuilder = new DbContextOptionsBuilder<ResellTicketDbContext>();
+            dbOptionsBuilder.UseSqlServer(_configuration.GetConnectionString("ResellTicketDB"));
+
+            _dataContext = new ResellTicketDbContext(dbOptionsBuilder.Options);
+            return _dataContext;
+        }
+
+        protected override void DisposeCore()
+        {
+            _dataContext?.Dispose();
+            base.DisposeCore();
+        }
+    }
+}
---
+++ Web/ResellTicketSolution/Core/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models
+{
+    public class Customer : EntityBase
+    {
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
