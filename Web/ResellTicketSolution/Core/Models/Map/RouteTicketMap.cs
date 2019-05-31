027654326c28ea2c51edc1ff8f47e3837b05a7dd
tienpp5695 <dark5695@gmail.com>
2019-05-31 13:50:44 +0700

    RT009.2: Edit Class RouteTicketMap

--- Web/ResellTicketSolution/Core/Models/Map/RouteTicketMap.cs
+++ Web/ResellTicketSolution/Core/Models/Map/RouteTicketMap.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Models.Map
 {
-    class RouteTicketMap
+    public class RouteTicketMap : IEntityTypeConfiguration<RouteTicket>
     {
+        public void Configure(EntityTypeBuilder<RouteTicket> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
     }
 }
