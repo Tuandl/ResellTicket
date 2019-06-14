f98a8ff2c31518238776b9f4b848fef214cbdd93
Tuan Duong <tuan@eatsyapp.co>
2019-06-14 21:51:17 +0700

    RT-022: tmp commit

---
+++ Web/ResellTicketSolution/ViewModel/ViewModel/Route/RouteSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ViewModel.ViewModel.Route
+{
+    public class RouteSearchViewModel
+    {
+        public double TotalAmount { get; set; }
+        public List<RouteTicketSearchViewModel> RouteTickets { get; set; }
+    }
+}
