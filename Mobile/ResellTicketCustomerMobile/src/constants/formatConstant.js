2cd908be0115132256273f9cd1ce3b695a141849
Tuan Duong <tuan@eatsyapp.co>
2019-06-20 12:08:21 +0700

    RT-021: Refactor Mobile structure

---
+++ Mobile/ResellTicketCustomerMobile/src/constants/formatConstant.js
@@ -0,0 +1,5 @@
+export default {
+    DATE_TIME: 'DDD, MMM dd, YYYY HH:mm',
+    DATE: 'DDD, MMM dd, YYYY',
+    TIME: 'HH:mm',
+}
--- Mobile/ResellTicketCustomerMobile/src/service/Api.js
+++ Mobile/ResellTicketCustomerMobile/src/service/Api.js
@@ -1,9 +1,24 @@
 import { create } from 'apisauce';
 import AppConfigs from '../config/appConfigs';
+import { AsyncStorage } from 'react-native';
+import keyConstant from '../constants/keyConstant';
+import NavigationService from './NavigationService';
+
+async function getToken() {
+    return await AsyncStorage.getItem(keyConstant.STORAGE.TOKEN);
+}
 
 const api = create({
     baseURL: AppConfigs.baseUrl,
-    
+    headers: {
+        Authorization: `Bearer ${getToken()}`,
+    }
+});
+
+api.addMonitor(response => {
+    if(response.status === 401) {
+        NavigationService.navigate('Login');
+    }
 });
 
 export default api;
