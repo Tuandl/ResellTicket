031fbc866188e23e94098f553317e6a98bac7a61
HuyHuy0933 <40488506+HuyHuy0933@users.noreply.github.com>
2019-06-20 23:29:06 +0700

    RT-061, RT062, RT063: Post, Edit, Delete Ticket

--- Web/ResellTicketSolution/WebClient.Customer/helper/Api.js
+++ Web/ResellTicketSolution/WebClient.Customer/helper/Api.js
@@ -1,12 +1,43 @@
-export const baseUrl = 'http://localhost:59152/';
+const baseUrl = 'http://localhost:59152/';
 
-export function Get(url) {
-    console.log(url);
+function Get(url) {
     return fetch(baseUrl + url, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json'                       
+        }
+    })
+}
+
+function Post(url, data){
+    return fetch(baseUrl + url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'                       
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+function Put(url, data) {
+    return fetch(baseUrl + url, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'                       
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+function Delete(url) {
+    return fetch(baseUrl + url, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'                       
         }
     })
 }
