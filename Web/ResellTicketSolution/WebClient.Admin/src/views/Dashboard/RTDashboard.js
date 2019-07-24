dc9c2b730d741735a461b30701de8d943c3270e5
HuyHuy0933 <40488506+HuyHuy0933@users.noreply.github.com>
2019-07-24 20:56:41 +0700

    RT-139: fix

--- Web/ResellTicketSolution/WebClient.Admin/src/views/Dashboard/RTDashboard.js
+++ Web/ResellTicketSolution/WebClient.Admin/src/views/Dashboard/RTDashboard.js
@@ -1,16 +1,8 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { Component } from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import {
-    Badge,
-    Button,
-    ButtonDropdown,
-    ButtonGroup,
-    ButtonToolbar,
     Card,
     CardBody,
-    CardFooter,
-    CardHeader,
-    CardTitle,
     Col,
     // Dropdown,
     // DropdownItem,
@@ -18,10 +10,9 @@ import {
     // DropdownToggle,
     // Progress,
     Row,
-    Table,
 } from 'reactstrap';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+import { getStyle } from '@coreui/coreui/dist/js/coreui-utilities'
 import CompletedRoute from '../routes/CompletedRoutes';
 import Axios from 'axios';
 
