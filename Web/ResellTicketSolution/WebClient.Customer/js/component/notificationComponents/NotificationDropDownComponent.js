752cb1a01d9a62723ca800d39a27fb334ccdbed8
THANHNVSE62560\vanth <vanthanh.nguyendn34@gmail.com>
2019-07-29 00:20:51 +0700

    empty noti

--- Web/ResellTicketSolution/WebClient.Customer/js/component/notificationComponents/NotificationDropDownComponent.js
+++ Web/ResellTicketSolution/WebClient.Customer/js/component/notificationComponents/NotificationDropDownComponent.js
@@ -39,7 +39,7 @@ export default class NotificationDropDownComponent {
             const resposneObj = await apiService.get(appConfig.apiUrl.notificationDataTable, params);
             this.data = resposneObj.data;
             this.total = resposneObj.total;
-        } catch(ex) {
+        } catch (ex) {
             toastService.error('An error occured when getting notification data.');
             console.error('Get Notification Data', ex);
         }
@@ -54,11 +54,11 @@ export default class NotificationDropDownComponent {
         divWrapper.appendChild(this.wrapperDropDownMenu);
 
         headerWrapper.appendChild(divWrapper);
-        
+
         commonService.removeAllChildren(this.containerElement);
         this.containerElement.appendChild(headerWrapper);
 
-        $('#notification-dropdown-container').scroll(this.onScrollNotification);  
+        $('#notification-dropdown-container').scroll(this.onScrollNotification);
     }
 
     generateDropDownButtonElement() {
@@ -71,14 +71,22 @@ export default class NotificationDropDownComponent {
     }
 
     generateDropDownMenuElement() {
-        const wrapper = commonService.htmlToElement(`<ul class="dropdown-menu text-left" id="notification-dropdown-container"></ul>`);
+        const wrapper = null;
+        if (this.data.length === 0) {
+            const wrapperEmpty = commonService.htmlToElement(`<ul class="dropdown-menu text-left" id="notification-dropdown-container" style="text-align:center;background-image: url(/images/empty_list_noti.png);background-size: 100% 100%;"></ul>`);
+            const row = new NotificationRowComponent("");
+            wrapperEmpty.appendChild(row.generateNotificationRowElementEmpty());
+            return wrapperEmpty;
+        } else {
+            const wrapper = commonService.htmlToElement(`<ul class="dropdown-menu text-left" id="notification-dropdown-container"></ul>`);
+            this.data.forEach(notification => {
+                const row = new NotificationRowComponent(notification);
+                wrapper.appendChild(row.generateNotificationRowElement());
+            });
+            return wrapper;
+        }
 
-        this.data.forEach(notification => {
-            const row = new NotificationRowComponent(notification);
-            wrapper.appendChild(row.generateNotificationRowElement());
-        });
 
-        return wrapper;
     }
 
     async loadMoreNotification() {
@@ -94,8 +102,8 @@ export default class NotificationDropDownComponent {
     }
 
     onScrollNotification() {
-        if($('#notification-dropdown-container').height() - $('#notification-dropdown-container').scrollTop() <= this.LOAD_MORE_THREADHOLD) {
-            if(this.page * this.pageSize < this.total && !this.isLoadingMore) {
+        if ($('#notification-dropdown-container').height() - $('#notification-dropdown-container').scrollTop() <= this.LOAD_MORE_THREADHOLD) {
+            if (this.page * this.pageSize < this.total && !this.isLoadingMore) {
                 this.isLoadingMore = true;
                 this.loadMoreNotification();
             }
